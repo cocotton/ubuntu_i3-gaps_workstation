5f7062824121fbfba460a9f1031535cfd70fbcc3
cocotton <julien.parent.trudeau@gmail.com>
2018-07-13 17:03:08 -0400

    Add metadata.rb

---
+++ metadata.rb
@@ -0,0 +1,12 @@
+name 'ubuntu_i3-gaps_workstation'
+maintainer 'Julien Parent-Trudeau'
+license 'Apache-2.0'
+description 'Installs/Configures ubuntu_i3-gaps_workstation'
+long_description 'Installs/Configures ubuntu-i3-gaps_workstation'
+version '0.1.0'
+chef_version '>= 12.1' if respond_to?(:chef_version)
+
+supports 'ubuntu'
+
+source_url 'https://github.com/cocotton/ubuntu_i3-gaps_workstation'
+issues_url 'https://github.com/cocotton/ubuntu_i3-gaps_workstation/issues'
