a1f2ae16ad9b1ffb5e251ff75349e89eaa51398d
cocotton <julien.parent.trudeau@gmail.com>
2018-07-15 12:48:33 -0400

    Compile, build and install i3-gaps

--- recipes/i3-gaps.rb
+++ recipes/i3-gaps.rb
@@ -1,3 +1,5 @@
+i3_repo_path = '/tmp/i3-gaps'
+
 # Install required packages listed here: https://github.com/Airblader/i3/wiki/Compiling-&-Installing#1610-
 %w[
   libxcb1-dev
@@ -25,4 +27,44 @@
 end
 
 # Clone the i3-gaps repository
-#git "/home/#{node['ubuntu_i3-gaps_workstation']['user']}/"
+git i3_repo_path do
+  repository 'https://github.com/Airblader/i3.git'
+  revision 'gaps-next'
+end
+
+# Generate the i3-gaps configuration files
+execute 'run_autoreconf' do
+  command 'autoreconf --force --install'
+  cwd i3_repo_path
+  live_stream true
+end
+
+# Delete the i3-gaps/build directory and its content if it exists
+directory "#{i3_repo_path}/build" do
+  recursive true
+  action :delete
+end
+
+# Create the i3-gaps/build directory
+directory "#{i3_repo_path}/build"
+
+# Configure i3-gaps before compiling it
+execute 'configure_i3-gaps' do
+  command '../configure --prefix=/usr --sysconfdir=/etc --disable-sanitizers'
+  cwd "#{i3_repo_path}/build"
+  live_stream true
+end
+
+# Compile i3-gaps
+execute 'compile_i3-gaps' do
+  command 'make'
+  cwd "#{i3_repo_path}/build"
+  live_stream true
+end
+
+# Install i3-gaps
+execute 'install_i3-gaps' do
+  command 'make install'
+  cwd "#{i3_repo_path}/build"
+  live_stream true
+end
