b16cd4d4d79b718180b29e4b245c9187b5fb7370
cocotton <julien.parent.trudeau@gmail.com>
2018-07-21 14:29:44 -0400

    Add termite attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,3 +2,7 @@ default['ubuntu_i3-gaps_workstation']['user'] = ''
 default['ubuntu_i3-gaps_workstation']['tmp_dir'] = "/home/#{node['ubuntu_i3-gaps_workstation']['user']}/Tmp"
 
 default['ubuntu_i3-gaps_workstation']['i3-gaps']['install'] = true
+default['ubuntu_i3-gaps_workstation']['i3-gaps']['repo_url'] = 'https://github.com/Airblader/i3.git'
+
+default['ubuntu_i3-gaps_workstation']['termite']['repo_url'] = 'https://github.com/thestinger/termite.git'
+default['ubuntu_i3-gaps_workstation']['termite']['vte-ng']['repo_url'] = 'https://github.com/thestinger/vte-ng.git'
