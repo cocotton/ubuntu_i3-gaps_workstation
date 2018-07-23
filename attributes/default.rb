ada47e8c2884d884821f012a753886311c115995
cocotton <julien.parent.trudeau@gmail.com>
2018-07-22 20:41:14 -0400

    Add polybar attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,6 +4,9 @@ default['ubuntu_i3-gaps_workstation']['tmp_dir'] = "/home/#{node['ubuntu_i3-gaps
 default['ubuntu_i3-gaps_workstation']['i3-gaps']['install'] = true
 default['ubuntu_i3-gaps_workstation']['i3-gaps']['repo_url'] = 'https://github.com/Airblader/i3.git'
 
+default['ubuntu_i3-gaps_workstation']['polybar']['install'] = true
+default['ubuntu_i3-gaps_workstation']['polybar']['repo_url'] = 'https://github.com/jaagr/polybar'
+
 default['ubuntu_i3-gaps_workstation']['termite']['install'] = true
 default['ubuntu_i3-gaps_workstation']['termite']['repo_url'] = 'https://github.com/thestinger/termite.git'
 default['ubuntu_i3-gaps_workstation']['termite']['vte-ng']['repo_url'] = 'https://github.com/thestinger/vte-ng.git'
