29a4ea7f645d9db6d310c885f821aecfc70cf22b
cocotton <julien.parent.trudeau@gmail.com>
2018-07-23 18:50:50 -0400

    Add playerctl attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,6 +7,9 @@ default['ubuntu_i3-gaps_workstation']['i3-gaps']['repo_url'] = 'https://github.c
 default['ubuntu_i3-gaps_workstation']['light']['install'] = true
 default['ubuntu_i3-gaps_workstation']['light']['repo_url'] = 'https://github.com/haikarainen/light.git'
 
+default['ubuntu_i3-gaps_workstation']['playerctl']['deb_package']['source'] = 'https://github.com/acrisci/playerctl/releases/download/v0.6.1/playerctl-0.6.1_amd64.deb'
+default['ubuntu_i3-gaps_workstation']['playerctl']['deb_package']['checksum'] = '2d03a17d4d9220e6fa2e71f9ba050ccbb90a321f84624b68c483f0769f4796f4'
+
 default['ubuntu_i3-gaps_workstation']['polybar']['install'] = true
 default['ubuntu_i3-gaps_workstation']['polybar']['repo_url'] = 'https://github.com/jaagr/polybar'
 
