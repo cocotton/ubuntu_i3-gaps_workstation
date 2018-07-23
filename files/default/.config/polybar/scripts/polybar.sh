67414f88b7486163cacf191ed922a9934ce7ca6e
cocotton <julien.parent.trudeau@gmail.com>
2018-07-23 17:07:11 -0400

    Update the polybar.sh script with the correct bars

--- files/default/.config/polybar/scripts/polybar.sh
+++ files/default/.config/polybar/scripts/polybar.sh
@@ -7,12 +7,12 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bars
-polybar top_DP-1-2 &
-polybar top_DP-2-2 &
-polybar top_eDP-1 &
-polybar bottom_DP-1-2 &
-polybar bottom_DP-2-2 &
-polybar bottom_eDP-1 &
+polybar top_DP-2-1 &
+polybar top_DP-2-2-8 &
+polybar top_DP-1-2-8 &
+polybar bottom_DP-2-1 &
+polybar bottom_DP-2-2-8 &
+polybar bottom_DP-1-2-8 &
 
 
 echo "Bars launched..."
