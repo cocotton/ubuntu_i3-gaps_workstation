682c0cac0fa2dd777b5b5a5ee446b0c6bab0c219
cocotton <julien.parent.trudeau@gmail.com>
2018-07-23 20:37:01 -0400

    Cleanup playerctl recipe and attributes

--- recipes/playerctl.rb
+++ recipes/playerctl.rb
@@ -1,10 +1,10 @@
 user = node['ubuntu_i3-gaps_workstation']['user']
 playerctl_deb_path = "#{node['ubuntu_i3-gaps_workstation']['tmp_dir']}/playerctl.deb"
+version = node['ubuntu_i3-gaps_workstation']['playerctl']['version']
 
 # Download playerctl deb package
 remote_file playerctl_deb_path do
-  source node['ubuntu_i3-gaps_workstation']['playerctl']['deb_package']['source']
-  checksum node['ubuntu_i3-gaps_workstation']['playerctl']['deb_package']['checksum']
+  source "https://github.com/acrisci/playerctl/releases/download/v#{version}/playerctl-#{version}_amd64.deb"
   owner user
   group user
   notifies :install, 'dpkg_package[install_playerctl]', :immediate
