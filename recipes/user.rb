3aa29a226e9c987c1d6d8ad7dc4cdafb86514a32
cocotton <julien.parent.trudeau@gmail.com>
2018-07-25 18:36:27 -0400

    Use the vagrant user instead of creating one

--- recipes/user.rb
+++
@@ -1,7 +0,0 @@
-user = node['ubuntu_i3-gaps_workstation']['user']
-
-# Dummy recipe used by test-kitchen to have a basic functional user
-user user do
-  home "/home/#{user}"
-  manage_home true
-end
